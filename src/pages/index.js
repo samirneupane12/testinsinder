0a17762f3fc2f2f9be8ebe4f0a9339cf86541310
samirneupane12 <91000829+samirneupane12@users.noreply.github.com>
2023-06-05 18:58:15 +0545

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Main from "./main";
 export default function Home() {
   return (
     
-    <div>Website is under maintenance</div>
+    <div style={{color:"white", fontSize:"30px", fontFamily:"roboto"}}>Website is under maintenance</div>
    
   );
 }
